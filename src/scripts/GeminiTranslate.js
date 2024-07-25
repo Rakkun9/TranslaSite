b1f31b51005c24a7d0c4d386bf096e1e71e8b025
Rakun <jnicolaybv@gmail.com>
2024-07-25 16:18:24 -0500

    refactor: Add GoogleGenerativeAI for translation functionality
    This commit adds the GoogleGenerativeAI library to enable translation functionality in the TranslaSite page. The library is imported from the '@google/generative-ai' package and is used to generate translations based on user input. This enhancement improves the user experience by providing accurate and efficient translations.

--- src/scripts/GeminiTranslate.js
+++ src/scripts/GeminiTranslate.js
@@ -1,3 +1,33 @@
-import { GeminiStart } from "./GeminiStart.js";
 
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
+const genAI = new GoogleGenerativeAI("AIzaSyB2mj4tS95ID7e0dUieqmnkBE4s6q-ved4");
+
+async function run(prompt){
+  
+    const model = genAI.getGenerativeModel({model:'gemini-1.5-flash'});
+
+    const preppedPrompt = 'Translate the following text into Spanish: ' + prompt;
+
+    const result = await model.generateContent(preppedPrompt);
+    const response = await result.response; 
+    const text = response.text(); 
+    console.log(text);
+    return text; 
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(
+    'input[type="button"][value="Traducir"]'
+  );
+  button.addEventListener("click", () => {
+    const texto1 = document.getElementById("textarea1").value;
+    console.log("Contenido del textarea 1:", texto1);
+
+    // Promp to generate the translation
+    let translation = run(texto1);
+    
+  });
+});
+
+   
