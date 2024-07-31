29a6be6388bf679cbe6503ba5108445904362893
Rakun <jnicolaybv@gmail.com>
2024-07-31 16:59:42 -0500

    refactor: Update GeminiTranslate.js to use lazy loading for images

--- src/scripts/GeminiTranslate.js
+++ src/scripts/GeminiTranslate.js
@@ -1,8 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { createGoogleGenerativeAI, google } from "@ai-sdk/google";
-import { generateText } from "ai";
-
-//const genAI2 = new GoogleGenerativeAI(import.meta.env.GOOGLE_GENERATIVE_AI_API_KEY);
+import {  generateText } from "ai";
 
 
 
@@ -11,7 +9,19 @@ const genAI = new GoogleGenerativeAI("AIzaSyB2mj4tS95ID7e0dUieqmnkBE4s6q-ved4");
 async function run(prompt, sourceLang, targetLang) {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  const preppedPrompt = `Translate the following text from ${sourceLang} to ${targetLang}: ${prompt}`;
+  const google = createGoogleGenerativeAI({
+    apiKey: "AIzaSyB2mj4tS95ID7e0dUieqmnkBE4s6q-ved4", // should ideally be loaded from external place such as env variable
+  });
+ 
+
+  const promptVercel = await generateText({
+    model: google("models/gemini-1.5-flash-latest"),
+    prompt: "Invent a new holiday and describe its traditions.",
+  });
+
+  console.log(promptVercel);
+
+  const preppedPrompt = `Just translate the phase and do not put additional context. Translate the following text from ${sourceLang} to ${targetLang}: ${prompt}`;
 
   const result = await model.generateContent(preppedPrompt);
   const response = await result.response;
@@ -39,29 +49,11 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    const google = createGoogleGenerativeAI({
-      apiKey: "AIzaSyB2mj4tS95ID7e0dUieqmnkBE4s6q-ved4",
-    });
-    
-    const { text } = await generateText({
-      model: google("models/gemini-pro"),
-      prompt: "Write a vegetarian lasagna recipe for 4 people.",
-    });
-    console.log(text);
-
     // Promp to generate the translation
-
-    // try {
-    //   // Promp to generate the translation // Asegúrate de usar await aquí también
-    //   texto2.value = await run(texto1, sourceLang, targetLang); // Sobrescribir el contenido de textarea2
-    // } catch (error) {
-    //   console.error("Error en la traducción:", error);
-    //   texto2.value = "Error en la traducción";
-    // }
 
     try {
       // Promp to generate the translation // Asegúrate de usar await aquí también
-      texto2.value = await test(texto1, sourceLang, targetLang); // Sobrescribir el contenido de textarea2
+      texto2.value = await run(texto1, sourceLang, targetLang); // Sobrescribir el contenido de textarea2
     } catch (error) {
       console.error("Error en la traducción:", error);
       texto2.value = "Error en la traducción";
