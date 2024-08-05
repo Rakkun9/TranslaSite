d9c58bf167b9c117eee8cfef5df6089477323584
Rakun <jnicolaybv@gmail.com>
2024-08-05 17:48:26 -0500

    refactor: Update astro.config.mjs to use vercelServerless adapter
    The astro.config.mjs file has been modified to update the adapter from "vercel" to "vercelServerless". This change ensures compatibility with the latest version of the "@astrojs/vercel" library and improves the overall functionality and performance of the application.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+// https://astro.build/config aaa
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercelServerLess()
+  adapter: vercelServerless()
 });
