0d116737068b358bad6993b8ab62c8d6ca106b9d
Rakun <jnicolaybv@gmail.com>
2024-08-05 17:46:09 -0500

    refactor: Update package.json to include @astrojs/vercel@7.7.2
    The package.json file has been modified to include the latest version of the "@astrojs/vercel" dependency. This update ensures compatibility with the latest features and improvements provided by the library.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node"
+
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercelServerLess()
 });
